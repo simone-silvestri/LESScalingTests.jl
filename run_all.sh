ca854040a376676cdc81fee22cd86b56cb1c240a
Simone Silvestri <silvestri.simone0@gmail.com>
2024-09-18 12:58:51 -0700

    adding new scaling

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,17 @@
 # RX=2 RY=2 ./run_les.sh
 # RX=4 RY=1 ./run_les.sh
 # RX=1 RY=4 ./run_les.sh
-RX=4 RY=2 ./run_les.sh
-RX=2 RY=4 ./run_les.sh
-RX=8 RY=1 ./run_les.sh
-RX=1 RY=8 ./run_les.sh
+# RX=4 RY=2 ./run_les.sh
+# RX=2 RY=4 ./run_les.sh
+# RX=8 RY=1 ./run_les.sh
+# RX=1 RY=8 ./run_les.sh
+# RX=16 RY=16 ./run_les.sh
+
+# RX=8 RY=8 ./run_les.sh
+# RX=32 RY=32 ./run_les.sh
+
+# RX=64 RY=1 ./run_les.sh
+# RX=16 RY=8 ./run_les.sh
+# RX=32 RY=16 ./run_les.sh
+# RX=64 RY=64 ./run_les.sh
+# RX=64 RY=32 ./run_les.sh
