0deb035aa77f994c36b1634a1a3b9535f99c189a
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-17 09:15:32 -0800

    final test

--- src/parse_results.jl
+++ src/parse_results.jl
@@ -25,43 +25,74 @@ function average_execution_time(ranks; folder = "./")
     return wall_time / tot_samples
 end
 
-function perlmutter_scaling_tests()
-    files = readdir("./")
-    files = filter(x -> length(x) > 8, files)
-    files = filter(x -> x[1:8] == "error_RX", files)
-    files = filter.(x -> '0'<=x<='9', files)
+function perlmutter_scaling_tests(dir)
+  files = readdir(dir)
+  files = filter(x -> length(x) > 8, files)
+  files = filter(x -> x[1:8] == "error_RX", files)
+  files = filter.(x -> '0'<=x<='9', files)
 
-    ranks = [(16, 1),
-	     (16, 16),
-	     (16,2),
-	     (16,8),
-	     (1,1),
-	     (1,2),
-	     (1,4),
-	     (1,8),
-	     (2,1),
-	     (2,16),
-	     (2,4),
-	     (32,16),
-	     (32,32),
-	     (4,1),
-	     (4,2),
-	     (64,1),
-	     (64,32),
-	     (64,64),
-	     (8,1),
-	     (8,2),
-	     (8,8)]
+  ranks = [(1 , 1 ), 
+           (2 , 1 ), 
+           (1 , 2 ), 
+           (2 , 2 ), 
+           (4 , 1 ), 
+           (1 , 4 ), 
+           (4 , 4 ), 
+           (8 , 1 ), 
+           (1 , 8 ), 
+           (8 , 8 ), 
+           (16, 1 ), 
+           (1 , 16), 
+           (16, 16), 
+           (4 , 2 ),   
+           (2 , 4 ),
+           (8 , 2 ),
+           (2 , 4 ),
+           (16, 2 ),
+           (2 , 16),
+	   (32, 32)]
+           # (64, 64)]
+         
+  sizes = Tuple(r .* (512, 512) for r in ranks)
+  times = zeros(length(ranks))
 
-    sizes = Tuple(r .* (512, 512) for r in ranks)
-    times = zeros(length(ranks))
+  for i in eachindex(ranks)
+      times[i] = average_execution_time(ranks[i]; folder = "./")
+  end
 
-    for i in eachindex(ranks)
-        times[i] = average_execution_time(ranks[i]; folder = "./")
-    end
+  return ranks, times
+end
 
-    rx = Float64[ranks[i][1] for i in eachindex(ranks)]
-    ry = Float64[ranks[i][2] for i in eachindex(ranks)]
+function visualize_results(dir="./data")
+   ranks, times = perlmutter_scaling_tests(dir)  
+   color = Float64.([1 in rank ? 0 : 1 for rank in ranks])
+   gpus  = Float64.([prod(rank) for rank in ranks])
 
-    return rx, ry, times
+   perm = sortperm(gpus)
+   color = color[perm]
+   times = times[perm]
+   gpus  = gpus[perm]
+
+   # color = color[2:end]
+   # times = times[2:end]
+   # gpus = gpus[2:end]
+
+   gputicks = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+   gpulabel = [L"1", L"2", L"4", L"8", L"16", L"32", L"64", L"128", L"256", L"512", L"1024"]
+  
+   fig = Figure(size = (1000, 300))
+   ax  = Axis(fig[1, 1], xlabel = L"\text{GPUs (Tesla A100)}", ylabel = L"\text{wall time per timestep (s)}", xscale = log2,
+	      xticks = (gputicks, gpulabel),
+	      yticks = ([4, 8, 12, 16, 20, 24], [L"4", L"8", L"12", L"16", L"20", L"24"]))
+   scatter!(ax, gpus, times; color, markersize=15)
+   lines!(ax, gpus, times, linewidth=0.5, linestyle=:dash)
+
+   ax  = Axis(fig[1, 2], xlabel = L"\text{GPUs (Tesla A100)}", ylabel = L"\text{Scaling efficiency [-]}", xscale = log2,
+	      xticks = (gputicks, gpulabel),
+	      yticks = ([0, 0.2, 0.4, 0.6, 0.8, 1], [L"0", L"0.2", L"0.4", L"0.6", L"0.8", L"1.0"]))
+   scatter!(ax, gpus, times[1] ./ times; color, markersize=15)
+   lines!(ax, gpus, times[1] ./ times, linewidth=0.5, linestyle=:dash)
+   ylims!(ax, 0, 1)
+
+   return fig
 end
